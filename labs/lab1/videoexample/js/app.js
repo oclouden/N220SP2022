238211bb46606601304294d1ed19d3af9b5a4411
oclouden <loudenowen@gmail.com>
2022-01-13 11:31:33 -0500

    First Demonstration

---
+++ labs/lab1/videoexample/js/app.js
@@ -0,0 +1,59 @@
+//console.log("It works");
+
+
+//practice example
+function setup() {
+    createCanvas(1000,1000);
+}
+function draw() {
+    circle(mouseX,mouseY,frameCount);
+}
+
+//function setup() {
+  //  createCanvas(800,600);  //width, height
+//}
+
+
+//Drawing ellipse
+    //use it in either setup() or draw()
+    //ellipse(x,y,width,height)
+
+    //function setup() {
+        //ellipse(50, 50 80 80 );
+    
+    //circle now (x,y,radius)
+      //  circle(30, 30 50);
+
+// x is horizontal, y is vertical, duh
+     
+    //rect(x,y,width,heigh)
+    //    rect(100, 100, 50, 20)
+
+    //the order determines who is on top, who is on bottom
+//}
+//making colors on the interior!!!!
+//fill("red") he doesnt like this
+//fill(213, 41, 67) 
+//RGB, HEX (#625785)
+//chaning edge color
+
+//stroke(color)
+//strokeWeight(radius)
+//noStroke(remove border)
+
+
+//drawing lines need strokes as well
+//line(x,y, starting point, end point)
+
+//duh, background color!
+//background(RGB)
+
+  //   var red = "#FF0000";
+    //    (could just put this inplace of any case needing red)
+
+
+
+
+      //  create a couple variables to establish things together
+
+        
